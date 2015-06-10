28e784383deda73e97c820852fd2326540721589
DuskyShelf <dupsdale@gmail.com>
2015-06-10 12:07:07 +0100

    updated rspec to check for working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
-describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+
+  describe DockingStation do
+    it { is_expected.to respond_to :release_bike }
+
+  decribe Bike do
+    it { is_ecpected.to respond_to :working? }
 end

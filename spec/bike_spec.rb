931b8d687716fa94271295d1972bd6905160e92d
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-11 11:31:28 +0100

    Creating and passing test to ensure a bike can be reported broken and then it is broken.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,12 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working? }
+
+  it { is_expected.to respond_to :report_broken }
+
+  it 'is broken if it has been reported as broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
+
 end

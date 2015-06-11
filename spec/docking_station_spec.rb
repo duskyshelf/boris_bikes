ee205bfce04b98f96891615aa7f869074c75d38d
DuskyShelf <dupsdale@gmail.com>
2015-06-11 14:16:12 +0100

    added test to makes sure dock doesn't release broken bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,6 +9,14 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it 'does not release broken bikes' do
+    bike = Bike.new
+    bike.report_broken
+    subject.dock bike
+    expect{ subject.release_bike }.to raise_error "No bikes available"
+  end
+
+
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end

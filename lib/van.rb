5ada9fc4de1c5cb5187137d30b166a4eb5f3e3d6
DuskyShelf <dupsdale@gmail.com>
2015-06-11 18:40:35 +0100

    checked for unload method on van

--- lib/van.rb
+++ lib/van.rb
@@ -2,4 +2,7 @@ require_relative 'bike_container'
 
 class Van
   include BikeContainer
+
+  def unload
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,4 +5,6 @@ describe Van do
 
   it_behaves_like BikeContainer
 
+  it { is_expected.to respond_to :unload }
+
 end

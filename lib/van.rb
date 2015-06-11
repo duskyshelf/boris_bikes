c20d2e7a5996de512956ba16bb85d9d72ce011c1
DuskyShelf <dupsdale@gmail.com>
2015-06-11 19:45:56 +0100

    added location variable to the Van class

--- lib/van.rb
+++ lib/van.rb
@@ -3,6 +3,25 @@ require_relative 'bike_container'
 class Van
   include BikeContainer
 
-  def unload
+  def initialize capacity = DEFAULT_CAPACITY
+    @bikes = []
+    @capacity = capacity
+    @location = :garage
   end
+
+  attr_accessor :location
+
+  def unload bike
+    remove_bike
+  end
+  def load bike
+    add_bike bike
+  end
+  def drive_to_garage
+    location = :garage
+  end
+  def drive_to_dock
+    location = :dock
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,7 +5,12 @@ describe Van do
 
   it_behaves_like BikeContainer
 
-  it { is_expected.to respond_to :unload }
-  it { is_expected.to respond_to :load }
+  it { is_expected.to respond_to(:unload).with(1).argument }
+  it { is_expected.to respond_to(:load).with(1).argument }
+
+  it 'can drive from dock to garage' do
+    subject.drive_to_garage
+    expect(subject.location).to eq :garage
+  end
 
 end
